95c03c2ea2cc7243a106474bd60fd80dec3de3f8
yashyc19 <yashyc19@gmail.com>
2024-04-14 20:09:45 +0530

    Managed gunicorn / main app

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
-from app.routes import app
+from flask_restful import Api
+from flask import Flask
+from app.resources.gupshup import GupshupAPI, GupshupWebhook
+
+app = Flask(__name__)
+api = Api(app)
+
+# Get sms data from gupshup via webhook endpoint
+api.add_resource(GupshupWebhook, '/gupshup/webhook')
+# Get and delete sms data from gupshupapi via API endpoint
+api.add_resource(GupshupAPI, '/gupshup/api', '/gupshup/api/<string:message_id>')
 
 
 if __name__ == '__main__':

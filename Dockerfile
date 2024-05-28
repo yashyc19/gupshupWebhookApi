a8608daf3c3c8e3f32316a03b148ae40127f30cc
Yash Chaube <ychaube@deloitte.com>
2024-05-28 14:42:32 +0530

    Status: Fixed - unnecessary sms concern

--- Dockerfile
+++ Dockerfile
@@ -1,51 +1,22 @@
-# syntax=docker/dockerfile:1
+# create a dockerfile to build the image
 
-# Comments are provided throughout this file to help you get started.
-# If you need more help, visit the Dockerfile reference guide at
-# https://docs.docker.com/go/dockerfile-reference/
+# Use an official Python runtime as a parent image
+FROM python:3.11.0-slim
+RUN apt-get update
+RUN apt update
 
-# Want to help us make this template better? Share your feedback here: https://forms.gle/ybq9Krt8jtBL3iCk7
+WORKDIR /usr/gupshupWebhookAPI
 
-ARG PYTHON_VERSION=3.12.2
-FROM python:${PYTHON_VERSION}-slim as base
+COPY requirements.txt ./
+RUN pip install --no-cache-dir -r requirements.txt
 
-# Prevents Python from writing pyc files.
-ENV PYTHONDONTWRITEBYTECODE=1
-
-# Keeps Python from buffering stdout and stderr to avoid situations where
-# the application crashes without emitting any logs due to buffering.
-ENV PYTHONUNBUFFERED=1
-
-WORKDIR /app
-
-# Create a non-privileged user that the app will run under.
-# See https://docs.docker.com/go/dockerfile-user-best-practices/
-ARG UID=10001
-RUN adduser \
-    --disabled-password \
-    --gecos "" \
-    --home "/nonexistent" \
-    --shell "/sbin/nologin" \
-    --no-create-home \
-    --uid "${UID}" \
-    appuser
-
-# Download dependencies as a separate step to take advantage of Docker's caching.
-# Leverage a cache mount to /root/.cache/pip to speed up subsequent builds.
-# Leverage a bind mount to requirements.txt to avoid having to copy them into
-# into this layer.
-RUN --mount=type=cache,target=/root/.cache/pip \
-    --mount=type=bind,source=requirements.txt,target=requirements.txt \
-    python -m pip install -r requirements.txt
-
-# Switch to the non-privileged user to run the application.
-USER appuser
-
-# Copy the source code into the container.
 COPY . .
 
-# Expose the port that the application listens on.
-EXPOSE 8000
 
-# Run the application.
-CMD gunicorn app:app
+# want to run this api on a standard port
+EXPOSE 80
+
+# run the command to start the api
+ENTRYPOINT [ "python" ]
+CMD [ "app.py" ]
+
--- src/resources/gupshup.py
+++ src/resources/gupshup.py
@@ -32,9 +32,6 @@ class GupshupWebhook(Resource):
         """
         Get sms data from Gupshup API, save it to a file, and return a standard output message.
         """
-        # Log the POST request
-        # gupshup_logger.log_request(request)
-        # get the query parameters, headers and body
         webhookdata = request.args
         if not any(webhookdata.values()):
             output = STD_OUTPUT.copy()
@@ -43,34 +40,32 @@ class GupshupWebhook(Resource):
             return output, 400
         
         datapath = config.WEBHOOK_DATA_FILE
-        # create the folders and file if it does not exist
         os.makedirs(os.path.dirname(datapath), exist_ok=True)
         try:
             if not os.path.exists(datapath):
                 with open(datapath, 'a') as f:
                     json.dump([], f)
     
-            # read the file
             with open(datapath, 'r') as f:
                 data = json.load(f)
             
-            # append the new data to the data list
-            data.append(webhookdata)
+            content = webhookdata.get('content')
+            # print(content)
+            if 'OTP' in content or 'otp' in content or 'One Time Password' in content:  # check if the message contains OTP
+                data.append(webhookdata)
+                with open(datapath, 'w') as f:
+                    json.dump(data, f)
+                output = STD_OUTPUT.copy()
+                output['message'] = 'Data saved to file - OTP message'
+                return output, 201
+            else:
+                print(f"Data received but not saved: {webhookdata}")
+                output = STD_OUTPUT.copy()
+                output['message'] = 'Data received but not saved'
+                return output, 200
     
-            # write the data back to the file
-            with open(datapath, 'w') as f:
-                json.dump(data, f)
-            
-            output = STD_OUTPUT.copy()
-            output['message'] = 'Data saved to file'
-            # Log the response
-            # Assuming you have a response object named `response`
-            # gupshup_logger.log_response(output)
-            print(data)
-            return output, 201
-        
         except Exception as e:
-            # gupshup_logger.error(f"Error saving data to file: {e}")
+            print(e)
             output = STD_OUTPUT.copy()
             output['status'] = 'error'
             output['message'] = 'Error saving data to file'
@@ -116,7 +111,8 @@ class GupshupAPI(Resource):
         # if messageid is not provided then return the whole data
         if message_id is None:
             output = STD_OUTPUT.copy()
-            output['message'] = 'Data found'
+            # message will be 'Data found' if data is present else 'No data at the moment' if the data is empty
+            output['message'] = 'Data found' if data else 'No data at the moment'
             output['data'] = data
             return output, 200
         else:

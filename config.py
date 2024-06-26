00e6526ee2abe197e36675cb76a918fdd2788b50
Yash Chaube <ychaube@deloitte.com>
2024-06-26 14:13:57 +0530

    Status: added multiple phone number integration

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 
 # Docker volume configuration (optional, customize paths)
 WEBHOOK_DATA_VOLUME = os.path.join(os.getcwd(), 'data')
-WEBHOOK_DATA_FILE = os.path.join(WEBHOOK_DATA_VOLUME, 'gupshup_webhook_data.json')
+WEBHOOK_DATA_FILE = 'webhook_data.json'
